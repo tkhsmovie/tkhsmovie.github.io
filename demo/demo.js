65d72439c8e6285ea366423acc5780293cbfb265
shouta0224 <shoutarouuuu@gmail.com>
2023-06-15 09:47:49 +0900

    1.8
    demoページ

---
+++ demo/demo.js
@@ -0,0 +1,12 @@
+document.getElementById("kirikae").style.visibility ="hidden";
+    
+function clickBtn(){
+    const kirikae = document.getElementById("kirikae");
+    
+    if(kirikae.style.visibility=="visible"){
+        kirikae.style.visibility ="hidden";
+    }
+    else{
+        kirikae.style.visibility ="visible";
+    }
+    }
